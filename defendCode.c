44542b0f5ad2c69cc8211a90c374e16c34505645
dtek <dtek210@gmail.com>
2020-08-01 08:13:03 -0700

    defendcode updated all bugs fixed program runs still need to handle passowrd prompt

--- defendCode.c
+++ defendCode.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <regex.h>
 #include <string.h>
 #include <stdbool.h>
 
-static char first_name[51];
-static char last_name[51];
-static char input_file_name[256];
-static char output_file_name[256];
-int first_number[16];
-int second_number[16];
+char first_name[51];
+char last_name[51];
+char input_file_name[256];
+char output_file_name[256];
+char *ptr;
+char first_number[16];
+char second_number[16];
 int sum, product, first, second;
 
 
-//this code is will
-
-void getNumbers();
 
 int validateLetterInput(char *s){
 	const char *valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -41,8 +40,8 @@ int validateFileNameInput(char *s){
 	return 0;
 }
 
-int validateIntInput(int *s){
-	const char *valid_chars = "123456789";
+int validateIntInput(char *s){
+	const char *valid_chars = "0123456789";
 	const char *invalid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/:*?<>|";
 	for(;*s;s++){
 		if(strchr(valid_chars, *s) == NULL){
@@ -58,31 +57,31 @@ int validateIntInput(int *s){
 int firstName(){
 	int i, c;
 	
-		puts("Please enter your first name: ");
-		scanf("%50s[a-zA-Z]", first_name);
+	puts("Please enter your first name: ");
+	scanf("%50s[a-zA-Z]", first_name);
 		
-		if(first_name == NULL){
-			printf("Invalid input!\n");
-			return 1; //no input
-		   }
-		if(first_name[strlen(first_name-1)] != '\n'){	
-				i = 0;
-			while(((c = getchar()) != '\n') && (c != EOF))
-				i = 1;
-		}
-		if(i == 1){
-			return 2; //input is longer then 50 characters
-		}
-		if(validateLetterInput(first_name) == 1){
-			return 3; //not all letters
-		}
+	if(first_name == NULL){
+		printf("Invalid input!\n");
+		return 1; //no input
+	}
+	if(first_name[strlen(first_name-1)] != '\n'){	
+		i = 0;
+	while(((c = getchar()) != '\n') && (c != EOF))
+		i = 1;
+	}
+	if(i == 1){
+		return 2; //input is longer then 50 characters
+	}
+	if(validateLetterInput(first_name) == 1){
+		return 3; //not all letters
+	}
 return 0; //input good
 }
 
 
 int lastName(){
 	int overflow, ch;
-	puts("Please enter your last name: ");
+	puts("\nPlease enter your last name: ");
 	scanf("%50s[a-zA-Z]", last_name);
 		
 	if(last_name == NULL){
@@ -105,12 +104,8 @@ return 0; //input good
 
 
 int firstInt(){
-
-	
-	puts("Please enter a first number: ");
-	//scanf("%10ls[0-9]", first_number);
-	
-	if(scanf("%ls0-9][^\n]", first_number) != 1){
+	puts("\nPlease enter first number: ");
+	if(scanf("%s0-9][^\n]", first_number) != 1){
 		fputs("wrong input. \n", stderr);
 		return 1;
 	}	
@@ -125,16 +120,15 @@ int firstInt(){
 	if(validateIntInput(first_number) == 1){
 		return 4; //not all letters
 	}
+	
+	first = strtol(first_number,&ptr, 10);
 	return 0;
 }
 
 
 int secondInt(){
-	int i,*number= 0;
-	puts("Please enter a second number: ");
-	
-	
-	if(scanf("%ls[0-9][^\n]", second_number) != 1){
+	puts("\nPlease enter second number: ");
+	if(scanf("%s[0-9][^\n]", second_number) != 1){
 		fputs("wrong input. \n", stderr);
 		return 1;
 	}	
@@ -149,166 +143,128 @@ int secondInt(){
 	if(validateIntInput(second_number) == 1){
 		return 4; //not all letters
 	}
-	
+	second = strtol(second_number, &ptr,10);
 	
 	return 0;
 }
 
 
 int fileOpen(){
-	int overflow, ch, i;
 	FILE *fp;
-	printf("Please enter file name: ");
-	
+	printf("\nPlease enter Output file name: ");
+	while (getchar() != '\n');
 	if(scanf("%50[a-zA-Z0-9\\.,_-]", output_file_name) != 1){
-	    printf("Entered name was: \"%s\"\n", output_file_name);
-        while (getchar() != '\n');
+	    //printf("Entered name was: \"%s\"\n", output_file_name);
+        
 		fputs("wrong input. \n", stderr);
 		return 1;
 	}
-//	if(output_file_name == NULL){
-//		printf("Invalid input!\n");
-//		return 1; //no input
-//	}
-//	if(output_file_name[strlen(output_file_name-1)] != '\n'){
-//		overflow = 0;
-//		while(((ch = getchar()) != '\n') && (ch != EOF))
-//			overflow = 1;
-//	}
-//	if(overflow == 1){
-//		return 2; //input is longer then 255 characters
-//	}
-//	if(validateFileNameInput(output_file_name) == 1){
-//		return 3; //not all letters
-//	}
-	
 	if((fp= fopen(output_file_name, "wt")) == NULL){
 		return 1;
 	}
-	fputs(last_name,fp);
-	fputs(first_name, fp);
-	//fputs(second_number, fp);
-	//fputs(first_number, fp);
 	
+	fprintf(fp, "%s , ", last_name);
+	fprintf(fp, "%s\n", first_name);
+	fprintf(fp, "First Number: %d\n", first);
+	fprintf(fp,"Second Number: %d\n", second);
+	fprintf(fp, "Sum of Numbers: %d\n", sum);
+	fprintf(fp,"Product of Numbers: %d\n", product);
 	fclose(fp);
 
 	printf("Input accepted\n");
-	printf("Your entry was: \"%s\"\n", output_file_name);
-	return 0;
+	printf("Your entry was written to: \"%s\"\n", output_file_name);
+return 0;
+}
+
+int fileRead(){
+        int c;
+	FILE *fp;
+	printf("\nPlease enter input file name: ");
+	while (getchar() != '\n');
+	if(scanf("%50[a-zA-Z0-9\\.,_-]", output_file_name) != 1){
+		fputs("wrong input. \n", stderr);
+		return 1;
+	}
+	if((fp= fopen(output_file_name, "r")) == NULL){
+	        printf("Cannot open file!!! ");
+		return 1;
+	}
+	printf("\n");
+	c = fgetc(fp);
+	while(c != EOF){
+	        printf("%c", c);
+	        c = fgetc(fp);
+	}
+        fclose(fp);
+return 0;
 }
 
 
-
-
-int firstNameHelper(){
+int getName(){
 	int i = 5;
 	do{
 		i = firstName();
-		printf("%d\n", i);
 	}while(i > 0);
-	return 0;
-}
-
-int lastNameHelper(){
-	int i = 5;
+        printf("Succesful: \n");	
+	i = 5;
 	do{
 		i = lastName();
-		printf("%d\n", i);
 	}while(i > 0);
-	return 0;
+        printf("Succesful: \n");
+return 0;
 }
 
-int fileOpenHelper(){
+int getFileOutput(){
 	int i = 5;
 	do{
 		i = fileOpen();
-		printf("%d\n", i);
 	}while(i > 0);
-	return 0;
+        printf("Succesful: \n");
+return 0;	
 }
 
-int firstIntHelper(){
+int getFileInput(){
+	int i = 5;
+	do{
+		i = fileRead();
+	}while(i > 0);
+        printf("Succesful: \n");
+return 0;	
+}
+
+int getNumber(){
 	int i = 5;
 	do{
 		i = firstInt();
-		printf("%d\n", i);
 	}while(i > 0);
-
-	return 0;
-}
-
-
-int secondIntHelper(){
-	int i = 5;
+        printf("Succesful: \n");
+	i = 5;
 	do{
 		i = secondInt();
-		printf("%d\n", i);
 	}while(i > 0);
-	return 0;
+        printf("Succesful: \n");
+return 0;
 }
 
-void intMath(int first, int second) {
-
-    product = first * second;
+void doMath() {
+    product = (int)first * (int)second;
+    sum = (int)first + (int)second;
 
 }
-main(void){
-//	firstNameHelper();
-//	lastNameHelper();
-//	firstIntHelper();
-//	secondIntHelper();
-//    getNumbers();
-	fileOpenHelper();
-	printf("Name: %s, %s\n", last_name, first_name);
-	printf("First Number: %d, ", first);
-	printf("Second Number: %d\n", second);
+
+int main(void){
+	getName();
+	getNumber();
+	doMath();
+	getFileOutput();
+	getFileInput();
+	//printf("\nName: %s, %s\n", last_name, first_name);
+	//printf("First Number: %d, ", first);
+	//printf("Second Number: %d\n", second);
+	//printf("Sum of Numbers: %d\n, ", sum);
+	//printf("Product of Numbers: %d\n", product);
 	
 return 0;
 }
 
-void getNumbers() {
 
-    bool valid = false;
-    do {
-        printf("Enter first number:\n");
-        int n = 0;
-        scanf("%d", &n);
-        getchar();
-        if (n == EOF) {
-            printf("EOF encountered\n");
-        } else if (n == 0) {
-            printf("Invalid input\n");
-        } else {
-            printf("Your number was: %d\n", n);
-            first = n;
-            printf("%d\n", first);
-            valid = true;
-        }
-    } while (!valid);
-
-    valid = false;
-
-    do {
-        printf("Enter second number:\n");
-        int n = 0;
-        scanf("%d", &n);
-        getchar();
-        if (n == EOF) {
-            printf("EOF encountered\n");
-        } else if (n == 0) {
-            printf("Invalid input\n");
-        } else {
-            printf("Your number was: %d\n", n);
-            second = n;
-            printf("%d\n", second);
-            valid = true;
-        }
-    } while (!valid);
-
-    sum = first + second;
-    product = first * second;
-
-    printf("Sum: %d\n", sum);
-    printf("Product: %d\n", product);
-
-}
